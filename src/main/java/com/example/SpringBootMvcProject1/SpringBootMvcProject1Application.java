8c45a545bb41ae400d41e428075566b7e859c729
Faiza Javed <faizafj@outlook.com>
2022-11-29 13:52:24 +0000

    files

---
+++ src/main/java/com/example/SpringBootMvcProject1/SpringBootMvcProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMvcProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcProject1Application.class, args);
+	}
+
+}
